a9caf848212c5a7a4818972e3a96fd18c3ff9824
smesnoje <anru1996@gmail.com>
2016-11-28 12:06:58 +0100

    stash

--- sites/all/themes/proba/templates/page.tpl.php
+++ sites/all/themes/proba/templates/page.tpl.php
@@ -157,7 +157,10 @@
 </div>
 
 <?php if (!empty($page['footer'])): ?>
+
   <footer class="footer <?php print $container_class; ?>">
     <?php print render($page['footer']); ?>
+    
+
   </footer>
 <?php endif; ?>
