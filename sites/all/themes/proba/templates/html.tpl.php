064c815d1c1de1db9ad5b0ea4f472dc44f5c5257
smesnoje <anru1996@gmail.com>
2016-12-15 15:17:08 +0100

    lkbasdnbas

--- sites/all/themes/proba/templates/html.tpl.php
+++ sites/all/themes/proba/templates/html.tpl.php
@@ -54,7 +54,6 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,700,900' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
